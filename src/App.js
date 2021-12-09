6879dc9d06f05c70f36aabf59570426ab467c921
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-12-09 12:11:13 +0530

    updating changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,84 @@
-import React from   "react";
+import React from "react";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <TemperatureInput/>
+      <h2>LIFTING STATE UP EXAMPLE</h2>
+      <Calculator />
     </div>
   );
 }
 
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
 const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit'
+  c: "Celsius",
+  f: "Fahrenheit"
 };
 
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = {temperature: ''};
+    this.state = { temperature: "" };
   }
 
   handleChange(e) {
-    this.setState({temperature: e.target.value});
+    // Before: this.setState({temperature: e.target.value});
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    // Before: const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
         <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
-               onChange={this.handleChange} />
+        <input value={temperature} onChange={this.handleChange} />
       </fieldset>
     );
   }
+}
+
+/**
+ Function to convert celsius to fahrenheit and vice versa
+ */
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+/** 
+Function that takes temperature as parameter and covert to string
+by round off the values.  
+*/
+function typConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
 }
